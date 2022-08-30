e094b51da90364ab11973cc4aafba7aeba293deb
John Olaer <62052631+jayoeychen@users.noreply.github.com>
2022-08-30 09:18:18 +0800

    Added BasicPay

--- Badyet.App/Pages/IncomePage.xaml.cs
+++ Badyet.App/Pages/IncomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.UI.Xaml;
+﻿using Badyet.App.ControlPages;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -20,6 +21,22 @@ namespace Badyet.App.Pages
         public IncomePage()
         {
             InitializeComponent();
+        }
+
+        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog dialog = new ContentDialog();
+
+            // XamlRoot must be set in the case of a ContentDialog running in a Desktop app
+            dialog.XamlRoot = this.XamlRoot;
+            dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+            dialog.Title = "Update basic pay?";
+            dialog.PrimaryButtonText = "Save";
+            dialog.CloseButtonText = "Cancel";
+            dialog.DefaultButton = ContentDialogButton.Primary;
+            dialog.Content = new UpdateContentDialog();
+
+            var result = await dialog.ShowAsync();
         }
     }
 }
