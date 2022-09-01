f119cacec8f447edde732e871a40cb89f5ed511a
John Olaer <62052631+jayoeychen@users.noreply.github.com>
2022-09-01 13:09:35 +0800

    PayoutPage Update.

---
+++ Badyet.App/Models/Payout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badyet.App.Models
+{
+    public class Payout
+    {
+        public DateOnly PayoutDate { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
--- Badyet.App/Pages/IncomePage.xaml.cs
+++ Badyet.App/Pages/IncomePage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Badyet.App.ControlPages;
-using Badyet.App.Models;
+﻿using Badyet.App.Models;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using System;
@@ -20,37 +19,22 @@ namespace Badyet.App.Pages
             BasicPay = 18_000m,
             Allowance = 3_600m,
         };
-        readonly string formattedBasicPay = $"{sampleIncome.BasicPay:C}";
-        readonly string formattedAllowance = $"{sampleIncome.Allowance:C}";
-        readonly string incomeType = sampleIncome.Type;
-        readonly string incomeName = sampleIncome.Name;
+
+        private readonly string formattedBasicPay = $"{sampleIncome.BasicPay:C}";
+        private readonly string formattedAllowance = $"{sampleIncome.Allowance:C}";
+        private readonly string incomeType = sampleIncome.Type;
+        private readonly string incomeName = sampleIncome.Name;
 
         static readonly decimal dailyPay = sampleIncome.BasicPay / 30;
         static readonly decimal annualPay = sampleIncome.BasicPay * 12;
 
-        readonly string formattedDailyPay = $"{dailyPay:C}";
-        readonly string formattedAnnualPay = $"{annualPay:C}";
+        private readonly string formattedDailyPay = $"{dailyPay:C}";
+        private readonly string formattedAnnualPay = $"{annualPay:C}";
 
         static readonly decimal dailyAllowance = sampleIncome.Allowance / 30;
         static readonly decimal annualAllowance = sampleIncome.Allowance * 12;
 
-        readonly string formattedDailyAllowance = $"{dailyAllowance:C}";
-        readonly string formattedAnnualAllowance = $"{annualAllowance:C}";
-
-
-        private async void AppBarButton_Click(object sender, RoutedEventArgs e)
-        {
-            ContentDialog dialog = new ContentDialog();
-
-            dialog.XamlRoot = this.XamlRoot;
-            dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
-            dialog.Title = "Update basic pay?";
-            dialog.PrimaryButtonText = "Save";
-            dialog.CloseButtonText = "Cancel";
-            dialog.DefaultButton = ContentDialogButton.Primary;
-            dialog.Content = new UpdateContentDialog();
-
-            var result = await dialog.ShowAsync();
-        }
+        private readonly string formattedDailyAllowance = $"{dailyAllowance:C}";
+        private readonly string formattedAnnualAllowance = $"{annualAllowance:C}";
     }
 }
