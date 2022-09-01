34563c838270de3a5b60d0edae7c70624a5509ca
John Olaer <62052631+jayoeychen@users.noreply.github.com>
2022-09-01 08:20:22 +0800

    Added PayPage.

---
+++ Badyet.App/ControlPages/PayPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Badyet.App.ControlPages
+{
+    public sealed partial class PayPage : Page
+    {
+        public PayPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
