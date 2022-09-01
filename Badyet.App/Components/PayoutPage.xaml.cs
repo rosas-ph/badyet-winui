279100c9d47b972f2993c18a6a36a62f00af237d
John Olaer <62052631+jayoeychen@users.noreply.github.com>
2022-09-01 13:49:20 +0800

    PayoutPage Update.

---
+++ Badyet.App/Components/PayoutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace Badyet.App.Components
+{ 
+    public sealed partial class PayoutPage : Page
+    {
+        public PayoutPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
