d29a76cc7c212ba7b62a5d87547360e0c6bf8ddd
John Olaer <62052631+jayoeychen@users.noreply.github.com>
2022-09-12 09:44:33 +0800

    Csharp focus design.

--- Badyet.App/Models/Income.cs
+++ Badyet.App/Models/Income.cs
@@ -2,8 +2,8 @@
 {
     public class Income
     {
-        public string Type { get; set; }
-        public string Name { get; set; }
+        public int ID { get; set; }
+        public string Employment { get; set; }
         public decimal BasicPay { get; set; }
         public decimal Allowance { get; set; }
         public decimal Bonus { get; set; } = 0;
--- Badyet.App/Pages/IncomePage.xaml.cs
+++ Badyet.App/Pages/IncomePage.xaml.cs
@@ -13,49 +13,5 @@ namespace Badyet.App.Pages
         {
             InitializeComponent();
         }
-
-        static readonly Income sampleIncome = new()
-        {
-            Type = "Employment",
-            Name = "Optum",
-            BasicPay = 18_000M,
-            Allowance = 3_600M,
-        };
-
-        private readonly string formattedBasicPay = $"{sampleIncome.BasicPay:C}";
-        private readonly string formattedAllowance = $"{sampleIncome.Allowance:C}";
-        private readonly string incomeType = sampleIncome.Type;
-        private readonly string incomeName = sampleIncome.Name;
-
-        static readonly decimal dailyPay = sampleIncome.BasicPay / 30;
-        static readonly decimal annualPay = sampleIncome.BasicPay * 12;
-
-        private readonly string formattedDailyPay = $"{dailyPay:C}";
-        private readonly string formattedAnnualPay = $"{annualPay:C}";
-
-        static readonly decimal dailyAllowance = sampleIncome.Allowance / 30;
-        static readonly decimal annualAllowance = sampleIncome.Allowance * 12;
-
-        private readonly string formattedDailyAllowance = $"{dailyAllowance:C}";
-        private readonly string formattedAnnualAllowance = $"{annualAllowance:C}";
-
-        private async void UpdateIncome_Click(object sender, RoutedEventArgs e)
-        {
-            ContentDialog dialog = new()
-            {
-                // XamlRoot must be set in the case of a ContentDialog running in a Desktop app
-                XamlRoot = this.XamlRoot,
-                Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style,
-                Title = "Update income",
-                PrimaryButtonText = "Save",
-                SecondaryButtonText = "Don't Save",
-                CloseButtonText = "Cancel",
-                DefaultButton = ContentDialogButton.Primary,
-                Content = new UpdateIncomeContentDialog()
-            };
-
-            var result = await dialog.ShowAsync();
-
-        }
     }
 }
